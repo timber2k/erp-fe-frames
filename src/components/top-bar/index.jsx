18919ef4da4e2dade453f5eba3eb45b07adf45dd
Tu (Timber) V. DOAN <timber@enclave.vn>
2023-05-16 14:57:22 +0700

    adding content and separate component

---
+++ src/components/top-bar/index.jsx
@@ -0,0 +1,70 @@
+import { Button, Layout, Menu } from "antd";
+import { AiOutlineArrowLeft, AiFillSetting } from "react-icons/ai";
+import styled from "styled-components";
+
+const TopBar = () => (
+  <StyledLayoutHeader>
+    <StyedMenu mode="horizontal" className="w-full font-semibold">
+      <Menu.Item key="1" icon={<AiOutlineArrowLeft />}>
+        Back to Courses
+      </Menu.Item>
+      <Menu.Item key="2">
+        <CourseName>Course Name</CourseName>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Status>Draft</Status>
+      </Menu.Item>
+      <Menu.Item key="4" style={{ marginLeft: "auto" }}>
+        <SaveButton>SAVE</SaveButton>
+      </Menu.Item>
+      <Menu.Item key="5">
+        <SettingIcon>
+          <AiFillSetting />
+        </SettingIcon>
+      </Menu.Item>
+    </StyedMenu>
+  </StyledLayoutHeader>
+);
+
+const StyledLayoutHeader = styled(Layout.Header)`
+  background: black;
+  border: 1px solid black;
+  border-top: 0px;
+  position: relative;
+  z-index: 10;
+`;
+
+const StyedMenu = styled(Menu)`
+  background: transparent;
+  color: white;
+  border: 1px solid black;
+  border-top: 0px;
+  font-size: 1rem;
+`;
+
+const CourseName = styled.span`
+  font-weight: bold;
+`;
+
+const Status = styled.span`
+  padding: 0.25rem;
+  margin: 0.25rem;
+  color: black;
+  font-weight: bold;
+  background: #8e8c8c;
+  border-radius: 0.2rem;
+`;
+
+const SaveButton = styled(Button)`
+  padding: 0.25rem;
+  margin: 0.25rem;
+  font-weight: bold;
+  background: #8e8c8c;
+  border-radius: 0.2rem;
+`;
+
+const SettingIcon = styled.div`
+  font-size: 2rem;
+`;
+
+export default TopBar;
--- src/pages/intended-learners/index.jsx
+++ src/pages/intended-learners/index.jsx
@@ -1,5 +1,52 @@
-const IntendedLearners = () => {
-  return <div>IntendedLearners</div>;
-};
+import { Layout } from "antd";
+import styled from "styled-components";
+import Sider from "antd/es/layout/Sider";
+
+import TopBar from "../../components/top-bar";
+import IntendedContent from "./content";
+
+const IntendedLearners = () => (
+  <ScreenLayout>
+    <TopBar />
+    <WrapperLayout>
+      <ContentLayout>
+        <IntendedContent />
+      </ContentLayout>
+      <SideBar width={300} style={{ background: "transparent" }}></SideBar>
+    </WrapperLayout>
+  </ScreenLayout>
+);
+
+const ScreenLayout = styled(Layout)`
+  width: 100%;
+  height: 100%;
+  background: white;
+  overflow: hidden;
+  border: 1px solid black;
+`;
+
+const WrapperLayout = styled(Layout)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  padding: 0 1rem;
+  padding-top: 5rem;
+  padding-bottom: 1rem;
+  display: flex;
+  border: 1px solid black;
+`;
+
+const ContentLayout = styled(Layout)`
+  width: auto;
+  margin-right: 1rem;
+  background: white;
+  border: 1px solid black;
+`;
+
+const SideBar = styled(Sider)`
+  background: white;
+  border: 1px solid black;
+  margin: 0;
+`;
 
 export default IntendedLearners;
