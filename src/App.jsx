2b72d811aec2714d5c656c4c1591f05729d93d58
Tu (Timber) V. DOAN <112548644+timber2k@users.noreply.github.com>
2023-05-23 13:35:23 +0700

    finish create course wizard (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import IntendedLearners from "./pages/intended-learners";
 import CourseMessages from "./pages/course-messages";
 import CourseLanding from "./pages/course-landing";
+import CreateCourseWizard from "./pages/create-course-wizard";
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
           <Route exact path="/intended-learners" component={IntendedLearners} />
           <Route exact path="/course-messages" component={CourseMessages} />
           <Route exact path="/course-landing" component={CourseLanding} />
+          <Route
+            exact
+            path="/create-course-wizard"
+            component={CreateCourseWizard}
+          />
         </Switch>
       </Router>
     </>
