159d5a2a82a3bf1bc9f6b6d4038b72adf830f2f2
Tu (Timber) V. DOAN <112548644+timber2k@users.noreply.github.com>
2023-06-02 09:57:25 +0700

    dashboard page complete (#10)

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import CourseMessages from "./pages/course-messages";
 import CourseLanding from "./pages/course-landing";
 import CreateCourseWizard from "./pages/create-course-wizard";
 import Curriculum from "./pages/curriculum";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
+          <Route exact path="/" component={Dashboard} />
           <Route exact path="/intended-learners" component={IntendedLearners} />
           <Route exact path="/course-messages" component={CourseMessages} />
           <Route exact path="/course-landing" component={CourseLanding} />
